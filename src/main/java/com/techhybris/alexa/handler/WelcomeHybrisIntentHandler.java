f66eae620d4167f53ff905e9d314229721e8614f
Anshul Garg <anshul@luminescent.digital>
2019-02-10 12:54:30 +0530

    Enabling slf4J in Alexa Hybris.

--- src/main/java/com/techhybris/alexa/handler/WelcomeHybrisIntentHandler.java
+++ src/main/java/com/techhybris/alexa/handler/WelcomeHybrisIntentHandler.java
@@ -1,22 +1,30 @@
 package com.techhybris.alexa.handler;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
-import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
+import com.techhybris.alexa.ingration.HybrisConnectorService;
 
 import java.util.Optional;
+import org.slf4j.Logger;
 
-import static com.amazon.ask.request.Predicates.intentName;
+import javax.annotation.Resource;
+
+import org.slf4j.LoggerFactory;
 
 public class WelcomeHybrisIntentHandler extends AbstractIntentHandler {
 
-    @Override
-    public Optional<Response> handle(HandlerInput input) {
-        String speechText = "Welcome Hybris";
-       return input.getResponseBuilder()
-                .withSpeech(speechText)
-                .withSimpleCard("WelcomeHybris", speechText)
-                .build();
-    }
+	private Logger LOG = LoggerFactory.getLogger(WelcomeHybrisIntentHandler.class);
+
+	@Resource(name = "hybrisConnectorService")
+	private HybrisConnectorService hybrisConnectorService;
+
+	@Override
+	public Optional<Response> handle(HandlerInput input) {
+		String speechText = "Welcome Hybris " + hybrisConnectorService.test();
+		// Logging sample example.
+		LOG.error(speechText);
+		System.out.println("SYSO: " + speechText);
+		return input.getResponseBuilder().withSpeech(speechText).withSimpleCard("WelcomeHybris", speechText).build();
+	}
 
 }
