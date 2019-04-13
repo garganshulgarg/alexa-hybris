72fc299f9ca1b3a63741412db540c853a6d92a3c
Anshul Garg <anshul@luminescent.digital>
2019-04-13 15:33:24 +0530

    Product Search Dynamic URL formation.

--- src/main/java/com/techhybris/alexa/integration/impl/DefaultHybrisProductSearchService.java
+++ src/main/java/com/techhybris/alexa/integration/impl/DefaultHybrisProductSearchService.java
@@ -1,10 +1,13 @@
 package com.techhybris.alexa.integration.impl;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,38 +19,46 @@ import com.techhybris.alexa.product.data.ProductSearchResult;
 
 public class DefaultHybrisProductSearchService implements HybrisProductSearchService {
 
+	private static final String FIELDS = "fields";
+
+	private static final String PAGE_SIZE = "pageSize";
+
+	private static final String QUERY = "query";
+
 	private Logger LOG = LoggerFactory.getLogger(DefaultHybrisProductSearchService.class);
-	
-	
-	@Resource(name="hybrisConnectivityClient")
+
+	@Resource(name = "hybrisConnectivityClient")
 	private HybrisConnectivityClient<ProductSearchResult> hybrisConnectivityClient;
-	
-	@Resource(name="hybrisConnectivityClient")
+
+	@Resource(name = "hybrisConnectivityClient")
 	private HybrisConnectivityClient<String> hybrisConnectivityClient1;
 
 	@Value("${hybris.base.url}")
 	private String hybrisBaseUrl;
-	
+
 	@Value("${hybris.base.site.uid}")
 	private String hybrisBaseSiteUid;
 
-
 	@Override
 	public ProductSearchResult findProducts(String accessToken, Map<String, String> params) {
-		if(StringUtils.isEmpty(accessToken))
-		{
+		if (StringUtils.isEmpty(accessToken)) {
 			return null;
-		}				
-		String url = hybrisBaseUrl + hybrisBaseSiteUid + "/products/search?query=camera&pageSize=4&fields=FULL";
-		HybrisRequest request = new HybrisRequest(accessToken);
-		request.setUrl(url);
-		Map<String, String> postData = request.getPostData();
-		postData.put("fields","FULL");
-		postData.put("pageSize","1");
-		postData.putAll(params);
-		return hybrisConnectivityClient.invokeRequest(request, ProductSearchResult.class);
+		}
+		try {
+			URI uri = new URIBuilder(hybrisBaseUrl + hybrisBaseSiteUid + "/products/search")
+										.addParameter(QUERY, null != params && null!=params.get(QUERY) ? params.get(QUERY) :StringUtils.EMPTY)
+										.addParameter(PAGE_SIZE, "4")
+										.addParameter(FIELDS, "FULL")
+										.build();
+			HybrisRequest request = new HybrisRequest(accessToken);
+			request.setUrl(uri.toString());
+			return hybrisConnectivityClient.invokeRequest(request, ProductSearchResult.class);
+
+		} catch (URISyntaxException e) {
+			LOG.error(e.getMessage());
+			return null;
+		}
+
 	}
-	
-	
 
 }
