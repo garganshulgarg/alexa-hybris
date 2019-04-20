ab790e4d226c3bc0c8c2693300e42db84f9c62b8
Anshul Garg <anshul@luminescent.digital>
2019-04-20 18:54:31 +0530

    AddToCart Slot details are added.

--- src/main/java/com/techhybris/alexa/handler/ProductSearchIntentHandler.java
+++ src/main/java/com/techhybris/alexa/handler/ProductSearchIntentHandler.java
@@ -1,14 +1,17 @@
 package com.techhybris.alexa.handler;
 
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.google.common.collect.Lists;
 import com.techhybris.alexa.integration.HybrisProductSearchService;
+import com.techhybris.alexa.product.data.Product;
 import com.techhybris.alexa.product.data.ProductSearchResult;
 
 public class ProductSearchIntentHandler extends AbstractIntentHandler {
@@ -25,9 +28,13 @@ public class ProductSearchIntentHandler extends AbstractIntentHandler {
 		LOG.error("Access Token: {}", accessToken);
 		
 		ProductSearchResult productSearchResult = hybrisProductSearchService.findProducts(accessToken, getSlots(input));
-		if(null != productSearchResult && CollectionUtils.isNotEmpty(productSearchResult.getProducts())) {
-			LOG.error("Setting session attributes of products: "+productSearchResult);
-			//setSessionAttributes(input, "productSearchResult", productSearchResult);
+		if(null != productSearchResult) {
+			List<String> productCodes = Lists.newArrayList();
+			for (Product product : productSearchResult.getProducts()) {
+				productCodes.add(product.getCode());
+			}
+			LOG.error("productCodes: {}", productCodes);
+			setSessionAttributes(input, "products", productCodes);
 		}
 		
 		addModel(input, "productSearchResult", productSearchResult);
